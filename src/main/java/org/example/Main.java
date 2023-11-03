bd7dd65abad1a482c4080c8d348d2a8fda43b098
TarCarbon <metalcarbon58@gmail.com>
2023-11-03 18:39:26 +0100

    config lod4j and add h2driver in gradle dependency

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.apache.log4j.Logger;
+
+public class Main {
+
+    final private static Logger logger = Logger.getLogger(Main.class);
+    public static void main(String[] args) {
+        logger.info("Started");
+
+    }
+}
